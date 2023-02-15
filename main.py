ea5f173eef804425317d1db40b7deb020bdbb81b
Tung Tran <tungts1101@gmail.com>
2023-02-15 12:01:22 +0900

    Test basic strategy for predators and preys.

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import sys
 import pygame
 import argparse
 import random
+import numpy as np
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-w', '--width', type=int)
 
 class Entity:
-    def __init__(self, id, screen_width, size=7, radius=20, speed=10, dir=(1, 1)):
+    def __init__(self, id, screen_width, size=7, radius=20, speed=10, dir=np.array([1, 1])):
         self.id = id
         self.screen_width = screen_width
         self.pos = (screen_width * random.random(), screen_width * random.random())
@@ -21,6 +22,9 @@ class Entity:
 
     def show(self):
         raise Exception("Missing implementation")
+    
+    def __eq__(self, __o: object) -> bool:
+        return self.id == __o.id
 
 def find_locals(entity, entities):
     local_preds = []
@@ -29,36 +33,61 @@ def find_locals(entity, entities):
         if entity.id == e.id: continue
         distance = ((entity.pos[0]-e.pos[0]) ** 2 + (entity.pos[1]-e.pos[1]) ** 2)
         if distance > entity.radius**2: continue
-        if isinstance(entity, Predator):
-            local_preds.append(entity)
-        elif isinstance(entity, Prey):
-            local_preys.append(entity)
+        if isinstance(e, Predator):
+            local_preds.append(e)
+        elif isinstance(e, Prey):
+            local_preys.append(e)
     return local_preds, local_preys
 
 class Predator(Entity):
-    def __init__(self, id, screen_width, size=7, radius=70, speed=4, dir=(1,1)):
-        super().__init__(id, screen_width, size, radius, speed)
+    def __init__(self, id, screen_width, time_alive=0, size=7, radius=70, speed=4, dir=np.array([1, 1])):
+        super().__init__(id, screen_width, size, radius, speed, dir)
+        self.time_alive = time_alive
     
     def update(self, interval, entities):
-        # self.dir = (random.random(), random.random())
+        self.time_alive += interval
         local_preds, local_preys = find_locals(self, entities)
-        print([e.id for e in local_preds])
+        removed_preys = []
+        distance = 1e6
+        for prey in local_preys:
+            distance_to_prey = (self.pos[0] - prey.pos[0]) ** 2 + (self.pos[1] - prey.pos[1]) ** 2
+            if distance > distance_to_prey:
+                distance = distance_to_prey
+                self.dir = np.array([prey.pos[0] - self.pos[0], prey.pos[1] - self.pos[1]])
+
+        norm = np.sqrt(self.dir.dot(self.dir))
+        self.dir = self.dir/(norm + (norm == 0))
 
         self.pos = (
             (self.pos[0] + self.dir[0]*self.speed) % self.screen_width, 
             (self.pos[1] + self.dir[1]*self.speed) % self.screen_width
         )
+        for prey in local_preys:
+            distance_to_prey = (self.pos[0] - prey.pos[0]) ** 2 + (self.pos[1] - prey.pos[1]) ** 2
+            if distance_to_prey < 10 ** 2:
+                removed_preys.append(prey)
+                self.time_alive -= 2000
+        
+        return removed_preys
 
     def show(self, screen):
         pygame.draw.circle(screen, (0,0,255), self.pos, self.size, 0)
         pygame.draw.circle(screen, (255,0,0), self.pos, self.radius, 1)     
 
 class Prey(Entity):
-    def __init__(self, id, screen_width, size=7, radius=100, speed=3, dir=(1,1)):
-        super().__init__(id, screen_width, size, radius, speed)
+    def __init__(self, id, screen_width, size=7, radius=100, speed=3, dir=np.array([1, 1])):
+        super().__init__(id, screen_width, size, radius, speed, dir)
     
     def update(self, interval, entities):
-        # self.dir = (random.random(), random.random())
+        local_preds, local_preys = find_locals(self, entities)
+        if local_preds:
+            pos = np.average([pred.pos for pred in local_preds], axis=0)
+            self.dir = np.array([-pos[0] + self.pos[0], -pos[1] + self.pos[1]])
+            norm = np.sqrt(self.dir.dot(self.dir))
+            self.dir = self.dir/(norm + (norm == 0))
+        else:
+            self.dir = np.array([0, 0])
+
         self.pos = (
             (self.pos[0] + self.dir[0]*self.speed) % self.screen_width, 
             (self.pos[1] + self.dir[1]*self.speed) % self.screen_width
@@ -81,11 +110,13 @@ if __name__ == '__main__':
     predators = []
     preys = []
     for _ in range(5):
-        predators.append(Predator(id, screen_width))
+        predators.append(Predator(id, screen_width, dir=np.array([random.randint(-1, 1), random.randint(-1, 1)])))
         id += 1
+    for _ in range(10):
         preys.append(Prey(id, screen_width))
         id += 1
 
+    passed_time = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT or pygame.key.get_pressed()[pygame.K_ESCAPE]: sys.exit()
@@ -93,9 +124,24 @@ if __name__ == '__main__':
         screen.fill(screen_color)
 
         interval = clock.tick(60)
-        entities = predators + preys
+        passed_time += interval
+        if passed_time > 1000:
+            passed_time = 0
+            for pred in predators:
+                pred.dir = np.array([random.randint(-1, 1), random.randint(-1, 1)])
+
+        removed_preds = set()
         for pred in predators:
-            pred.update(interval, entities)
+            if pred.time_alive > 5000:
+                removed_preds.add(pred.id)
+        predators = [pred for pred in predators if pred.id not in removed_preds]
+
+        entities = predators + preys
+        removed_preys = set()
+        for pred in predators:
+            for prey in pred.update(interval, entities):
+                removed_preys.add(prey.id)
+        preys = [prey for prey in preys if prey.id not in removed_preys]
         for prey in preys:
             prey.update(interval, entities)
 
